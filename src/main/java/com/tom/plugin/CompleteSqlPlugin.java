5b853abf8a502d5603bb2f92f491d449223f5bfb
tomxin <863970612@qq.com>
2019-03-11 22:24:53 +0800

    [add] 新增打印完整SQL的逻辑

--- src/main/java/com/tom/plugin/CompleteSqlPlugin.java
+++ src/main/java/com/tom/plugin/CompleteSqlPlugin.java
@@ -1,11 +1,17 @@
 package com.tom.plugin;
 
 import org.apache.ibatis.executor.Executor;
+import org.apache.ibatis.mapping.BoundSql;
 import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.mapping.ParameterMapping;
+import org.apache.ibatis.mapping.ParameterMode;
 import org.apache.ibatis.plugin.*;
+import org.apache.ibatis.reflection.MetaObject;
 import org.apache.ibatis.session.ResultHandler;
 import org.apache.ibatis.session.RowBounds;
 
+import java.util.Date;
+import java.util.List;
 import java.util.Properties;
 
 /**
@@ -32,6 +38,7 @@ public class CompleteSqlPlugin implements Interceptor {
 
         try {
             // 打印日志逻辑
+
 
         } catch (Exception e) {
 
@@ -66,7 +73,42 @@ public class CompleteSqlPlugin implements Interceptor {
      * @param invocation
      */
     private void printCompleteSql(Invocation invocation) {
+        // 获取对应的参数值
         Object[] args = invocation.getArgs();
         MappedStatement mappedStatement = (MappedStatement) args[0];
+        Object parameter = args[1];
+        // 获取SQL
+        BoundSql boundSql = mappedStatement.getBoundSql(parameter);
+        String originalSql = boundSql.getSql();
+        List<ParameterMapping> parameterMappings = boundSql.getParameterMappings();
+        // 判空
+        if (null == parameterMappings || parameterMappings.size() <= 0) {
+            return;
+        }
+        for (int i = 0; i < parameterMappings.size(); ++i) {
+            ParameterMapping parameterMapping = parameterMappings.get(0);
+            // 判断参数的类型是否是输入的类型
+            if (parameterMapping.getMode() == ParameterMode.OUT) {
+                continue;
+            }
+            String propertyName = parameterMapping.getProperty();
+            Object value;
+            if (boundSql.hasAdditionalParameter(propertyName)) {
+                value = boundSql.getAdditionalParameter(propertyName);
+            } else if (null == parameter) {
+                value = null;
+            } else {
+                MetaObject metaObject = mappedStatement.getConfiguration().newMetaObject(parameter);
+                value = metaObject.getValue(propertyName);
+            }
+            // 获取数据类型
+            if (parameterMapping.getJavaType().equals(Date.class)) {
+                value = PluginDateUtils.formatToString((Date) value);
+            }
+            originalSql.replaceFirst("\\?", value.toString());
+        }
+        System.out.println("=======================complete sql start=============================");
+        System.out.println("tom plugin print complete sql [ " + originalSql + " ]");
+        System.out.println("=======================complete sql end===============================");
     }
 }
---
+++ src/main/java/com/tom/plugin/PluginDateUtils.java
@@ -0,0 +1,22 @@
+package com.tom.plugin;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class PluginDateUtils {
+
+    private static String DATE_FORMAT = "yyyy-MM-dd hh:mm:ss";
+
+    private static SimpleDateFormat SIMPLE_DATE_FORMAT = new SimpleDateFormat(DATE_FORMAT);
+
+    /**
+     * 将时间转换为字符串类型
+     *
+     * @param date
+     * @return
+     */
+    public static String formatToString(Date date) {
+
+        return SIMPLE_DATE_FORMAT.format(date);
+    }
+}
